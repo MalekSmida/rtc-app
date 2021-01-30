e1fbf47f82776b72091e0baabf4feab37cc3f44e
Malek Smida <msmida96@gmail.com>
2021-01-30 16:48:59 +0100

    @InitProject

---
+++ public/script.js
@@ -0,0 +1,52 @@
+const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: "3001",
+});
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+
+    socket.on("call", (call) => {
+      call.answer(stream);
+    });
+
+    socket.on("user-connected", (userId) => {
+      console.log("connected!");
+      connectToNewUser(userId, stream);
+    });
+  });
+
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+function connectToNewUser(userId, stream) {
+  // call another user by passing his id <<userId>>
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+  // remove user video when he/she disconnect
+  call.on("close", () => {
+    video.remove();
+  });
+}
+
+// inject the stream in the video and play it
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
